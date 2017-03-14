6e0910d492acabde8af2213ec72896498979bd47
harrison <harrison@129.57.113.245>
2017-03-13 21:33:17 -0400

    bunch of stuff

--- drawface.sh
+++ drawface.sh
@@ -6,5 +6,7 @@ javac src/parts/Eye.java
 javac src/parts/Nose.java
 javac src/parts/Mouth.java
 javac -cp src/ src/scratch/Face.java
-java -Xmx1536m -Xms1024m -cp src/ scratch.Face
+javac -cp src/ src/scratch/Test1.java
+#java -Xmx1536m -Xms1024m -cp src/ scratch.Face
+java -Xmx1536m -Xms1024m -cp src/ scratch.Test1
 
